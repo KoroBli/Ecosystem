15a2258fffd5457e58578083ea739c15d4318618
Jordi <jjorba95@gmail.com>
2019-10-16 02:07:14 +0200

    Changed FOV system (WIP)
    The previous FOV system was lame.

--- Ecosystem/Assets/Scripts/FirstParticle.cs
+++ Ecosystem/Assets/Scripts/FirstParticle.cs
@@ -10,6 +10,8 @@ public class FirstParticle : MonoBehaviour
     RaycastHit[] rayHit; // Deteccion de colision
     public int vRange; // Distancia de vision
     float dPRay; // Distancia entre rayos
+    public bool foodFound;
+    public GameObject food;
 
     // Start is called before the first frame update
     void Start()
@@ -20,22 +22,24 @@ public class FirstParticle : MonoBehaviour
         rayHit = new RaycastHit[vResolution];
 
         vRange = 15;
-        life = 100.0f;
-
-        for (int i = 0; i < vRay.Length; i++)
-        {
-            vRay[i].origin = this.transform.position;
-            vRay[i].direction = new Vector3((dPRay * i) - 1, 0, 1);
-        }
+        life = 10000.0f;
+        foodFound = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        life -= 0.1f;
+        /*for (int i = 0; i < vRay.Length; i++)
+        {
+            vRay[i].origin = this.transform.position;
+            vRay[i].direction = new Vector3(transform.forward.x + (dPRay * i) - 1, transform.forward.y, transform.forward.z);
+        }*/
+
+        life -= 0.01f;
 
         //Debug.Log("Life: " + life);
         Debug.Log("Distance: " + dPRay);
+        Debug.Log("Direction: " + transform.forward.x);
         if (life <= 0)
         {
             Destroy(this.gameObject);
@@ -48,13 +52,17 @@ public class FirstParticle : MonoBehaviour
         {
             Debug.DrawRay(vRay[i].origin, vRay[i].direction * vRange);
 
-            if (Physics.Raycast(vRay[i], out rayHit[i], vRange))
+            //if(Physics.SphereCast(vRay[i], )
+
+            /*if (Physics.Raycast(vRay[i], out rayHit[i], vRange))
             {
                 if (rayHit[i].collider.gameObject.tag == "Food")
                 {
                     Debug.Log("Food!");
+                    foodFound = true;
+                    food = rayHit[i].collider.gameObject;
                 }
-            }
+            }*/
         }
     }
 
