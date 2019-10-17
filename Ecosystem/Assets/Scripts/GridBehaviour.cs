92320d76e60a7e7a166df0cfd5f4001b5a9dc53b
Jordi <jjorba95@gmail.com>
2019-10-17 12:05:31 +0200

    Implementing A* algorythm for pathfinding
    Work in Progress

---
+++ Ecosystem/Assets/Scripts/GridBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridBehaviour : MonoBehaviour
+{
+    public LayerMask unwalkableMask;
+    public Vector2 gridWorldSize;
+    public float nodeRadius;
+    Node[,] grid;
+
+    float nodeDiameter;
+    int gridSizeX, gridSizeY;
+
+    private void Start()
+    {
+        nodeDiameter = nodeRadius * 2;
+        gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+        gridSizeY = Mathf.RoundToInt(gridWorldSize.Y / nodeDiameter);
+        CreateGrid();
+    }
+
+    void CreateGrid()
+    {
+        grid = new Node[gridSizeX, gridSizeY];
+        Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.forward * gridWorldSize.y / 2;
+
+        for(int x = 0; x < gridSizeX; x++)
+        {
+            for (int y = 0; y < gridSizeY; y++)
+            {
+                Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y))
+    }
+}
---
+++ Ecosystem/Assets/Scripts/Node.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node
+{
+    public bool walkable;
+    public Vector3 worldPosition;
+
+    public Node(bool m_walkable, Vector3 m_worldPosition)
+    {
+        walkable = m_walkable;
+        worldPosition = m_worldPosition;
+    }
+}
