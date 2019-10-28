602b62e9637fe5ba45eb0fd178d42ace747b7a04
Jordi <jjorba95@gmail.com>
2019-10-28 22:49:20 +0100

    WIP pathfinding smoothness

---
+++ Ecosystem/Assets/Scripts/Line.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Line
+{
+    const float verticalLineGradient = 1e5f; // Lo igualamos a 10 elevado a 5
+
+    float gradient;
+    float y_Intercept;
+
+    float gradientPerpendicular;
+
+    public Line(Vector2 pointOnLine, Vector2 pointPerpendicularToLine)
+    {
+        float dx = pointOnLine.x - pointPerpendicularToLine.x;
+        float dy = pointOnLine.y - pointPerpendicularToLine.y;
+
+        gradientPerpendicular = dy / dx;
+
+
+    }
+}
