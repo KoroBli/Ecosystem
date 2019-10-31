e56e68f6048d6456330526e71eb42d6af20e1106
Jordi <jjorba95@gmail.com>
2019-10-31 01:17:15 +0100

    Improving pathfinding

--- Ecosystem/Assets/Scripts/Unit.cs
+++ Ecosystem/Assets/Scripts/Unit.cs
@@ -4,44 +4,79 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    const float minPathUpdateTime = .2f;
+    const float pathUpdateMoveThreshhold = .5f;
+
     public Transform target;
-    float speed = 1;
-    Vector3[] path;
-    int targetIndex;
+    public float speed = 10;
+    public float turnSpeed = 3;
+    public float turnDst = 5;
+
+    Path path;
 
     private void Start()
     {
-        PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
+        StartCoroutine(UpdatePath());
     }
 
-    public void OnPathFound(Vector3[] newPath, bool pathSuccessful)
+    public void OnPathFound(Vector3[] wayPoints, bool pathSuccessful)
     {
         if(pathSuccessful)
         {
-            path = newPath;
+            path = new Path(wayPoints, transform.position, turnDst);
             StopCoroutine("FollowPath");
             StartCoroutine("FollowPath");
         }
     }
 
-    IEnumerator FollowPath()
+    IEnumerator UpdatePath()
     {
-        Vector3 currentWaypoint = path[0];
+        if(Time.timeSinceLevelLoad < .3f)
+        {
+            yield return new WaitForSeconds(.3f);
+        }
+        PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
+
+        float squareMoveThreshhold = pathUpdateMoveThreshhold * pathUpdateMoveThreshhold;
+        Vector3 targetPosOld = target.position;
 
         while(true)
         {
-            if(transform.position == currentWaypoint)
+            yield return new WaitForSeconds(minPathUpdateTime);
+            if ((target.position - targetPosOld).sqrMagnitude > squareMoveThreshhold)
             {
-                targetIndex++;
-                if(targetIndex >= path.Length)
-                {
-                    yield break;
-                }
+                PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
+                targetPosOld = target.position;
+            }
+        }
+    }
 
-                currentWaypoint = path[targetIndex];
+    IEnumerator FollowPath()
+    {
+        bool followingPath = true;
+        int pathIndex = 0;
+        transform.LookAt(path.lookPoints[0]);
+
+        while(followingPath)
+        {
+            Vector2 pos2D = new Vector2(transform.position.x, transform.position.z);
+            while(path.turnBoundaries[pathIndex].HasCrossedLine(pos2D))
+            {
+                if (pathIndex == path.finishLineIndex)
+                {
+                    followingPath = false;
+                    break;
+                }
+                else pathIndex++;
             }
 
-            transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
+            if(followingPath)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(path.lookPoints[pathIndex] - transform.position);
+                transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime - turnSpeed);
+                transform.Translate(Vector3.forward * Time.deltaTime * speed, Space.Self);
+            }
+
             yield return null;
         }
     }
